c9baca5cac3064de0888df72518b5d06d6f1f287
Jacob Albright <jacob.albright23@gmail.com>
2018-12-07 23:50:14 -0600

    set up FlaskSQLAchemy with sqlite database

--- application/app.py
+++ application/app.py
@@ -1,15 +1,40 @@
 from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 
-app = Flask(__name__)
+def get_args(req, *args):
+  return { arg for arg in req.get_json() if arg in args}
 
-app.config['JWT_SECRET_KEY'] = '3ncl20fnd3o95'
-jwt = JWTManager(app)
 
-@app.route('/', methods=["GET"])
-def login():
-  return 'Hi'
+def create_app():
+  app = Flask(__name__)
+
+  from .db import init_db_command, db
+
+  app.config['JWT_SECRET_KEY'] = '$00p3R_$3Cr3t'
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+  app.config['DEBUG'] = True
+  
+  jwt = JWTManager(app)
+
+  db.init_app(app)
+
+  app.cli.add_command(init_db_command)
+
+  @app.route('/register', methods=["POST"])
+  def register():
+    password, username = get_args(request, 'username', 'password').values()
+    print('password', password, 'username', username)
+
+
+  @app.route('/hi', methods=["GET"])
+  def login():
+    print('hello')
+    return jsonify({'msg': 'Hi'})
+
+  return app
 
 
 if __name__ == '__main__':
+  app = create_app()
   app.run(debug=True, host='0.0.0.0')
